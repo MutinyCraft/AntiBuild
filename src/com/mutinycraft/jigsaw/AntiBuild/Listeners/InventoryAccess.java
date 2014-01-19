0138c24beda9900cd8807660800635b297ff1e1d
MutinyCraft <admin@mutinycraft.com>
2014-01-19 11:50:29 -0600

    Implement InventoryAccess listener.

--- src/com/mutinycraft/jigsaw/AntiBuild/Listeners/InventoryAccess.java
+++ src/com/mutinycraft/jigsaw/AntiBuild/Listeners/InventoryAccess.java
@@ -1,7 +1,12 @@
 package com.mutinycraft.jigsaw.AntiBuild.Listeners;
 
 import com.mutinycraft.jigsaw.AntiBuild.AntiBuild;
+import com.mutinycraft.jigsaw.AntiBuild.Util.PlayerMessenger;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryOpenEvent;
 
 /**
  * Author: Jigsaw
@@ -32,5 +37,92 @@ public class InventoryAccess implements Listener {
 
     public InventoryAccess(AntiBuild p) {
         plugin = p;
+    }
+
+    /**
+     * Checks if player has proper permission to interact with inventory types.
+     *
+     * @param event
+     */
+    @EventHandler(priority = EventPriority.LOW)
+    public void NoInventoryAccess(InventoryOpenEvent event) {
+        Player player = null;
+
+        if (event.getPlayer() instanceof Player) {
+            player = (Player) event.getPlayer();
+        }
+        if (player != null && !player.hasPermission("antibuild.bypass") && !player.hasPermission("antibuild" +
+                ".interact")) {
+            switch (event.getInventory().getType()) {
+                case ANVIL:
+                    if (!player.hasPermission("antibuild.anvil")) {
+                        event.setCancelled(true);
+                        PlayerMessenger.messageHandler(plugin.getConfigHandler().getNoInteractMessage(), player);
+                    }
+                    break;
+                case BEACON:
+                    if (!player.hasPermission("antibuild.beacon")) {
+                        event.setCancelled(true);
+                        PlayerMessenger.messageHandler(plugin.getConfigHandler().getNoInteractMessage(), player);
+                    }
+                    break;
+                case BREWING:
+                    if (!player.hasPermission("antibuild.brewing")) {
+                        event.setCancelled(true);
+                        PlayerMessenger.messageHandler(plugin.getConfigHandler().getNoInteractMessage(), player);
+                    }
+                    break;
+                case CHEST:
+                    if (!player.hasPermission("antibuild.chest")) {
+                        event.setCancelled(true);
+                        PlayerMessenger.messageHandler(plugin.getConfigHandler().getNoInteractMessage(), player);
+                    }
+                    break;
+                case CRAFTING:
+                    if (!player.hasPermission("antibuild.crafting")) {
+                        event.setCancelled(true);
+                        PlayerMessenger.messageHandler(plugin.getConfigHandler().getNoInteractMessage(), player);
+                    }
+                    break;
+                case DISPENSER:
+                    if (!player.hasPermission("antibuild.dispenser")) {
+                        event.setCancelled(true);
+                        PlayerMessenger.messageHandler(plugin.getConfigHandler().getNoInteractMessage(), player);
+                    }
+                    break;
+                case ENCHANTING:
+                    if (!player.hasPermission("antibuild.enchanting")) {
+                        event.setCancelled(true);
+                        PlayerMessenger.messageHandler(plugin.getConfigHandler().getNoInteractMessage(), player);
+                    }
+                    break;
+                case ENDER_CHEST:
+                    if (!player.hasPermission("antibuild.enderchest")) {
+                        event.setCancelled(true);
+                        PlayerMessenger.messageHandler(plugin.getConfigHandler().getNoInteractMessage(), player);
+                    }
+                    break;
+                case HOPPER:
+                    if (!player.hasPermission("antibuild.hopper")) {
+                        event.setCancelled(true);
+                        PlayerMessenger.messageHandler(plugin.getConfigHandler().getNoInteractMessage(), player);
+                    }
+                    break;
+                case FURNACE:
+                    if (!player.hasPermission("antibuild.furnace")) {
+                        event.setCancelled(true);
+                        PlayerMessenger.messageHandler(plugin.getConfigHandler().getNoInteractMessage(), player);
+                    }
+                    break;
+                case WORKBENCH:
+                    if (!player.hasPermission("antibuild.workbench")) {
+                        event.setCancelled(true);
+                        PlayerMessenger.messageHandler(plugin.getConfigHandler().getNoInteractMessage(), player);
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
