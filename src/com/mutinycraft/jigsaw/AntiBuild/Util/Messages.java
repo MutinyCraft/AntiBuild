5a4a7936af62ce1d3e4afca8b00bb11dbc0d349f
MutinyCraft <admin@mutinycraft.com>
2014-05-24 20:31:51 -0500

    Added file to handle static messages.

---
+++ src/com/mutinycraft/jigsaw/AntiBuild/Util/Messages.java
@@ -0,0 +1,35 @@
+package com.mutinycraft.jigsaw.AntiBuild.Util;
+
+import org.bukkit.ChatColor;
+
+/**
+ * Author: Jigsaw
+ * *
+ * Date: 5/24/2014
+ * *
+ * Copyright 2014 MutinyCraft
+ * *
+ * This file is part of AntiBuild.
+ * *
+ * AntiBuild is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * *
+ * AntiBuild is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * *
+ * You should have received a copy of the GNU General Public License
+ * along with AntiBuild.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+public class Messages {
+
+    public static final String HELP = ChatColor.translateAlternateColorCodes('&', "&c---- AntiBuild Command Help ----" +
+            " \n&a/antibuild reload &b: Reloads the plugin. \n&a/antibuild lock [world] &b: Locks the provided world." +
+            " \n&a/antibuild unlock [world] &b: Unlocks the provided world.\n&a/antibuild list &b: Shows the " +
+            "currently locked worlds.");
+
+}
